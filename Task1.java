8968b7eb148cc7e6af2e6d41a20dc68264dd9921
Laisan13 <74246711+Laisan13@users.noreply.github.com>
2023-08-03 20:12:13 +0300

    homework Java_lesson_1  JDK11  Task 1 and 2

---
+++ Task1.java
@@ -0,0 +1,15 @@
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Иванов Валерий Петрович");
+        System.out.println("50 лет");
+        System.out.println("рост 175 см");
+        System.out.println();
+        System.out.println("Шахова Ирина Васильевна");
+        System.out.println("25 лет");
+        System.out.println("рост 162 см");
+        System.out.println();
+        System.out.println("Торгашова Елена Сергеевна");
+        System.out.println("39 лет");
+        System.out.println("рост 158 см");
+        }
+}
---
+++ Task2.java
@@ -0,0 +1,5 @@
+public class Task2 {
+    public static void main(String[] args) {
+        System.out.printf("%.2f %.2f %.2f", 3.222, 4.333, 5.444);
+    }
+}
